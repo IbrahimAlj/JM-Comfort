c845d0b07451136a1cac9e7c6c273b1c9246f9a1
Ammar <ammartaha99@gmail.com>
2025-10-19 16:23:01 -0700

    Added JM Comfort logo and favicon to header

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Welcome to JM Comfort</h1>
+      </main>
+    </>
+  );
+}
