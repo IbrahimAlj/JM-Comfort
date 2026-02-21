51646e5088b425e3381ef255a9f16a23830c4d27
ColeHollandsworth <colehollandsworthcs@gmail.com>
2026-02-21 00:02:19 -0800

    JMHABIBI #90 Integrates feature cards onto the bottom of the homepage. Cards render correctly, offering some brief descriptions of features, that can be easily changed based on the formating of sub-task #89

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import HeroBanner from '../components/HeroBanner';
 import Navbar from '../components/Navbar';
+import WhyChooseUs from '../components/WhyChooseUsCard';
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <HeroBanner />
+      <WhyChooseUs />
 
       {/* Add other homepage sections below /}
       {/ Services preview, testimonials, etc. */}
