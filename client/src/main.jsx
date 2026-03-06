48fbdc5e7b1d8fab05845d8be4340d668f45244a
Ammar <ammartaha99@gmail.com>
2026-03-05 22:21:47 -0800

    JMHABIBI-183: add keyboard focus styles and fix focus visibility

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import './index.css'
+import './focus-styles.css'
 import App from './App.jsx'
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HelmetProvider>
